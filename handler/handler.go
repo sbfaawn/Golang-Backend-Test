d55413cf5a889c6f5c37da6787df1d968a4e8694
sbfaawn <andhikarizki00000@gmail.com>
2023-02-10 08:27:12 +0700

    dev phase 8 add log

--- handler/handler.go
+++ handler/handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"Golang-Backend-Test/helper"
+	"Golang-Backend-Test/logger"
 	"Golang-Backend-Test/model/request"
 	response2 "Golang-Backend-Test/model/response"
 	"Golang-Backend-Test/service"
@@ -12,18 +13,24 @@ import (
 )
 
 func HealthCheckHandler(c echo.Context) error {
+	defer logger.Log("Health Check Endpoint is End", false)
+	logger.Log("Health Check Endpoint is Called", false)
 	response := response2.Response{Message: "Project is Up"}
 	return c.JSON(http.StatusOK, response)
 }
 
 // /
 func CreateUser(c echo.Context) error {
+	defer logger.Log("Create User Endpoint is End", false)
+	logger.Log("Create User Endpoint is Called", false)
 	o := new(request.User)
 	if err := c.Bind(o); err != nil {
+		logger.Log("Error Occured when Create User : "+err.Error(), true)
 		return err
 	}
 
 	if err := service.InsertUser(o); err != nil {
+		logger.Log("Error Occured when Create User : "+err.Error(), true)
 		return err
 	}
 
@@ -36,16 +43,20 @@ func CreateUser(c echo.Context) error {
 }
 
 func CreateOrderItem(c echo.Context) error {
+	defer logger.Log("Create Order Item Endpoint is End", false)
+	logger.Log("Create Order Item Endpoint is Called", false)
 	o := new(request.OrderItem)
 	if err := c.Bind(o); err != nil {
 		return err
 	}
 
 	if err := helper.IsValidDate(o.ExpiredAt); err != nil {
+		logger.Log("Error Occured when Create Order Item : "+err.Error(), true)
 		return err
 	}
 
 	if err := service.InsertOrderItem(o); err != nil {
+		logger.Log("Error Occured when Create Order Item : "+err.Error(), true)
 		return err
 	}
 
@@ -58,13 +69,17 @@ func CreateOrderItem(c echo.Context) error {
 }
 
 func CreateOrderHistory(c echo.Context) error {
+	defer logger.Log("Create Order History Endpoint is End", false)
+	logger.Log("Create Order History Endpoint is Called", false)
 	o := new(request.OrderHistory)
 	if err := c.Bind(o); err != nil {
+		logger.Log("Error Occured when Create Order History : "+err.Error(), true)
 		return err
 	}
 
 	err := service.InsertOrderHistory(o)
 	if err != nil {
+		logger.Log("Error Occured when Create Order History : "+err.Error(), true)
 		return err
 	}
 
@@ -79,8 +94,11 @@ func CreateOrderHistory(c echo.Context) error {
 ///
 
 func ListUsers(c echo.Context) error {
+	defer logger.Log("List User Endpoint is End", false)
+	logger.Log("List User Endpoint is Called", false)
 	users, err := service.GetUsers()
 	if err != nil {
+		logger.Log("Error Occured when List User : "+err.Error(), true)
 		return err
 	}
 
@@ -93,8 +111,11 @@ func ListUsers(c echo.Context) error {
 }
 
 func ListOrderItems(c echo.Context) error {
+	defer logger.Log("List Order Item Endpoint is End", false)
+	logger.Log("List Order Item Endpoint is Called", false)
 	orderItems, err := service.GetOrderItems()
 	if err != nil {
+		logger.Log("Error Occured when List Order History : "+err.Error(), true)
 		return err
 	}
 
@@ -107,8 +128,11 @@ func ListOrderItems(c echo.Context) error {
 }
 
 func ListOrderHistories(c echo.Context) error {
+	defer logger.Log("List Order History Endpoint is End", false)
+	logger.Log("List Order Hsitory Endpoint is Called", false)
 	orderHistories, err := service.GetOrderHistories()
 	if err != nil {
+		logger.Log("Error Occured when List Order History : "+err.Error(), true)
 		return err
 	}
 
@@ -122,14 +146,18 @@ func ListOrderHistories(c echo.Context) error {
 
 // /
 func DetailUser(c echo.Context) error {
+	defer logger.Log("Detail Order Item Endpoint is End", false)
+	logger.Log("Detail Order Item Endpoint is Called", false)
 	paramId := c.QueryParam("id")
 	idInt, err := strconv.Atoi(paramId)
 	if err != nil {
+		logger.Log("Error Occured when Get Detail User : "+err.Error(), true)
 		return err
 	}
 
 	user, err := service.GetUserById(&idInt)
 	if err != nil {
+		logger.Log("Error Occured when Get Detail User : "+err.Error(), true)
 		return err
 	}
 
@@ -142,14 +170,18 @@ func DetailUser(c echo.Context) error {
 }
 
 func DetailOrderItem(c echo.Context) error {
+	defer logger.Log("Detail Order Item Endpoint is End", false)
+	logger.Log("Detail Order Item Endpoint is Called", false)
 	paramId := c.QueryParam("id")
 	idInt, err := strconv.Atoi(paramId)
 	if err != nil {
+		logger.Log("Error Occured when Get Detail Order Item : "+err.Error(), true)
 		return err
 	}
 
 	orderItem, err := service.GetOrderItemById(&idInt)
 	if err != nil {
+		logger.Log("Error Occured when Get Detail Order Item : "+err.Error(), true)
 		return err
 	}
 
@@ -162,14 +194,18 @@ func DetailOrderItem(c echo.Context) error {
 }
 
 func DetailOrderHistory(c echo.Context) error {
+	defer logger.Log("Detail Order History Endpoint is End", false)
+	logger.Log("Detail Order History Endpoint is Called", false)
 	paramId := c.QueryParam("id")
 	idInt, err := strconv.Atoi(paramId)
 	if err != nil {
+		logger.Log("Error Occured when Get Detail Order History : "+err.Error(), true)
 		return err
 	}
 
 	orderHistory, err := service.GetOrderHistoriesById(&idInt)
 	if err != nil {
+		logger.Log("Error Occured when Get Detail Order History : "+err.Error(), true)
 		return err
 	}
 
@@ -183,17 +219,22 @@ func DetailOrderHistory(c echo.Context) error {
 
 // /
 func UpdateUser(c echo.Context) error {
+	defer logger.Log("Update User Endpoint is End", false)
+	logger.Log("Update User Endpoint is Called", false)
 	o := new(request.User)
 	if err := c.Bind(o); err != nil {
+		logger.Log("Error Occured when Update User : "+err.Error(), true)
 		return err
 	}
 
 	if o.Id == nil {
+		logger.Log("Error Occured when Update User : Id need to be specified in request", true)
 		return errors.New("Id need to be specified in request")
 	}
 
 	user, err := service.UpdateUser(o)
 	if err != nil {
+		logger.Log("Error Occured when Update User : "+err.Error(), true)
 		return err
 	}
 
@@ -206,17 +247,22 @@ func UpdateUser(c echo.Context) error {
 }
 
 func UpdateOrderItem(c echo.Context) error {
+	defer logger.Log("Update Order Item Endpoint is End", false)
+	logger.Log("Update Order Item Endpoint is Called", false)
 	o := new(request.OrderItem)
 	if err := c.Bind(o); err != nil {
+		logger.Log("Error Occured when Update Order Item : "+err.Error(), true)
 		return err
 	}
 
 	if o.Id == nil {
+		logger.Log("Error Occured when Update Order Item : Id need to be specified in request", true)
 		return errors.New("Id need to be specified in request")
 	}
 
 	orderItem, err := service.UpdateOrderItem(o)
 	if err != nil {
+		logger.Log("Error Occured when Update Order Item : "+err.Error(), true)
 		return err
 	}
 
@@ -228,17 +274,22 @@ func UpdateOrderItem(c echo.Context) error {
 }
 
 func UpdateOrderHistory(c echo.Context) error {
+	defer logger.Log("Update Order History Endpoint is End", false)
+	logger.Log("Update Order History Endpoint is Called", false)
 	o := new(request.OrderHistory)
 	if err := c.Bind(o); err != nil {
+		logger.Log("Error Occured when Update Order History : "+err.Error(), true)
 		return err
 	}
 
 	if o.Id == nil {
+		logger.Log("Error Occured when Update History : Id need to be specified in request", true)
 		return errors.New("Id need to be specified in request")
 	}
 
 	orderHistory, err := service.UpdateOrderHistory(o)
 	if err != nil {
+		logger.Log("Error Occured when Update Order History : "+err.Error(), true)
 		return err
 	}
 
@@ -251,14 +302,18 @@ func UpdateOrderHistory(c echo.Context) error {
 
 // /
 func DeleteUser(c echo.Context) error {
+	defer logger.Log("Delete User Endpoint is End", false)
+	logger.Log("Delete User Endpoint is Called", false)
 	paramId := c.QueryParam("id")
 	idInt, err := strconv.Atoi(paramId)
 	if err != nil {
+		logger.Log("Error Occured when Delete User : "+err.Error(), true)
 		return err
 	}
 
 	err = service.SoftDeleteUser(&idInt)
 	if err != nil {
+		logger.Log("Error Occured when Delete User : "+err.Error(), true)
 		return err
 	}
 
@@ -270,14 +325,18 @@ func DeleteUser(c echo.Context) error {
 }
 
 func DeleteOrderItem(c echo.Context) error {
+	defer logger.Log("Delete Order Item Endpoint is End", false)
+	logger.Log("Delete Order Item Endpoint is Called", false)
 	paramId := c.QueryParam("id")
 	idInt, err := strconv.Atoi(paramId)
 	if err != nil {
+		logger.Log("Error Occured when Delete Order Item : "+err.Error(), true)
 		return err
 	}
 
 	err = service.SoftDeleteOrderItem(&idInt)
 	if err != nil {
+		logger.Log("Error Occured when Delete Order Item : "+err.Error(), true)
 		return err
 	}
 
---
+++ logger/logger.go
@@ -0,0 +1,17 @@
+package logger
+
+import (
+	"log"
+	"os"
+)
+
+func Log(message string, isError bool) {
+	f, err := os.OpenFile("testlogfile", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+
+	log.SetOutput(f)
+	log.Println(message)
+}
